4696dc88c0c99e71f723116d63a0cd1c58c146f3
DanielVesterbaek <daniel.v.jensen@gmail.com>
2024-05-07 14:31:42 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@ bl_info = {
     "category": "Object",
     "author": "Daniel Vesterbaek, Gabriela Rodriguez, Blastframe",
     "blender": (4, 1, 0),
-    "version": (1, 6, 0),
+    "version": (1, 6, 1),
     "location": "Viewport 'N-Panel'",
     "description": "Rig, Animate and Calculate Physics for your Vehicles!"    
     }
